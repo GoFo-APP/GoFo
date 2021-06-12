576f3277389c6dc0cbd0e38b1d5cc216192e8bbc
RaniaAlaa17 <84988903+RaniaAlaa17@users.noreply.github.com>
2021-06-12 20:00:39 +0200

    Add files via upload

---
+++ Lists.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gofo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Zainab
+ */
+public class Lists {
+
+    ArrayList<Playground> playgroundList = new ArrayList<>();
+    ArrayList<User> playgroundOwnerList = new ArrayList<>();
+    ArrayList<Player> playerList = new ArrayList<>();
+
+    public Lists() {
+    }
+}
